d1d5f4d53c04415984b5c3bc2968ef793d82043d
frustak <soulburn007@gmail.com>
2023-02-04 18:35:20 +0330

    add theme config to unocss

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,6 +1,25 @@
 import { presetMini } from "@unocss/preset-mini"
+import presetWebFonts from "@unocss/preset-web-fonts"
 import { defineConfig } from "@unocss/vite"
 
 export default defineConfig({
-	presets: [presetMini()],
+	presets: [
+		presetMini(),
+		presetWebFonts({
+			provider: "google",
+			fonts: {
+				body: "Eczar",
+			},
+		}),
+		{
+			theme: {
+				colors: {
+					brand: {
+						text: "#323031",
+						secondary: "#FFC857",
+					},
+				},
+			},
+		},
+	],
 })
