566c94f4ea09bdb65723d5f5f9120562c4945434
frustak <soulburn007@gmail.com>
2023-02-24 01:03:54 +0330

    fix input label name

--- src/ui/compound.tsx
+++ src/ui/compound.tsx
@@ -1,5 +1,5 @@
 import { Field, FieldPath, FieldValues, FormState } from "@modular-forms/solid"
-import { ComponentProps, Show } from "solid-js"
+import { ComponentProps } from "solid-js"
 import { Input } from "../ui/simple"
 
 type InputFieldProps<
@@ -19,19 +19,10 @@ export const InputField = <
 ) => (
 	<Field of={props.of} name={props.name}>
 		{(field) => (
-			<div class="flex flex-col">
-				<Show when={props.label}>
-					<label for={field.name} class="text-sm">
-						{props.label}
-					</label>
-				</Show>
-				<Input
-					id={field.name}
-					{...props}
-					{...field.props}
-					value={field.value?.toString()}
-				/>
-			</div>
+			<label class="flex flex-col">
+				<span class="text-sm">{props.label}</span>
+				<Input {...props} {...field.props} value={field.value?.toString()} />
+			</label>
 		)}
 	</Field>
 )
