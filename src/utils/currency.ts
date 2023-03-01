f7c04efca95be22cab16c2d7cf03cbd29e919d96
frustak <soulburn007@gmail.com>
2023-03-01 20:25:39 +0330

    handle format currency errors

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,9 +1,21 @@
+import { settings } from "../features/settings/store"
+
 function formatCurrency(value: number) {
-	return Intl.NumberFormat("en-US", {
-		style: "currency",
-		currency: "USD",
-		maximumFractionDigits: 0,
-	}).format(value)
+	let formatted = value.toString()
+	try {
+		formatted = Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: settings()?.currency ?? "USD",
+			maximumFractionDigits: 0,
+		}).format(value)
+	} catch (error) {
+		formatted = Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: "USD",
+			maximumFractionDigits: 0,
+		}).format(value)
+	}
+	return formatted
 }
 
 export { formatCurrency }
