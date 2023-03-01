e55faf999aad08671acf9af0196e2c2ae8563558
frustak <soulburn007@gmail.com>
2023-03-01 20:25:47 +0330

    add link to currency codes

--- src/features/settings/form.tsx
+++ src/features/settings/form.tsx
@@ -2,20 +2,25 @@ import { createForm, Form, zodForm } from "@modular-forms/solid"
 import { Component } from "solid-js"
 import { z } from "zod"
 import { InputField } from "../../ui/compound"
-import { Button } from "../../ui/simple"
+import { Anchor, Button } from "../../ui/simple"
 import { setSettings, settings } from "./store"
 
 export const SettingsForm: Component = () => {
 	return (
 		<Form of={settingsForm} onSubmit={submitForm} class="flex justify-between items-end">
-			<InputField
-				of={settingsForm}
-				name="currency"
-				label="Currency"
-				placeholder="Currency"
-				type="text"
-				required
-			/>
+			<div class="grow flex flex-col gap-2">
+				<InputField
+					of={settingsForm}
+					name="currency"
+					label="Currency"
+					placeholder="Currency"
+					type="text"
+					required
+				/>
+				<Anchor href="https://www.iban.com/currency-codes" class="self-start">
+					Currency codes
+				</Anchor>
+			</div>
 			<Button type="submit">Save</Button>
 		</Form>
 	)
