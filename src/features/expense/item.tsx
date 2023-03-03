5371d1f99f3ae787dd6694833c12549c563fa322
frustak <soulburn007@gmail.com>
2023-03-03 20:56:47 +0330

    add category to expense

--- src/features/expense/item.tsx
+++ src/features/expense/item.tsx
@@ -11,7 +11,12 @@ export const ExpenseItem: Component<{ expense: Expense }> = (props) => {
 	return (
 		<div class="flex justify-between">
 			<div class="flex flex-col gap-1">
-				<p class="bg-brand-red/50 self-start">{formatMoney(props.expense.amount)}</p>
+				<div class="flex items-center gap-2">
+					<p class="bg-brand-green/50">{formatMoney(props.expense.amount)}</p>
+					<p class="text-brand-text/70 text-xs bg-brand-text/5">
+						{props.expense.category}
+					</p>
+				</div>
 				<p class="text-sm">{props.expense.description}</p>
 			</div>
 			<div class="flex flex-col items-end gap-1">
